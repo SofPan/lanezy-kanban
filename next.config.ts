be8714bbe0448f2d5c5ec238dfab2bd732c0b86c
Sofia Panchaud <s.panchaud.89@gmail.com>
2026-02-03 19:03:26 -0500

    add prisma client to next.config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push({
+        '@prisma/client': '@prisma/client',
+      });
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
